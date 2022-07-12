573ae4b6035ab7954e3c8cc49c46be881a43c883
rqthomas <rqthomas@vt.edu>
2022-07-12 15:09:26 -0400

    generalizing cycle

--- hourly.R
+++ hourly.R
@@ -16,30 +16,23 @@ s3 <- arrow::s3_bucket("drivers/noaa/neon/gefs",
                        endpoint_override =  "data.ecoforecast.org",
                        anonymous=TRUE)
 
-
-
 df <- arrow::open_dataset(s3)
 
 dates <- seq(lubridate::as_date("2022-04-20"), lubridate::as_date("2022-04-22"), by = "1 day")
-cycle <- c("00:00:00")
-
-
-
+cycles <- c("00")
 
 sites <- df |> 
-  dplyr::filter(start_time == lubridate::as_datetime(forecast_start_times[1]),
+  dplyr::filter(start_time == lubridate::as_datetime(dates[1]),
                 variable %in% c("PRES","TMP","RH","UGRD","VGRD","APCP","DSWRF","DLWRF")) |> 
   distinct(site_id) |> 
   collect() |> 
   pull(site_id)
 
-forecast_start_times <- expand.grid(dates, cycle, sites) |> 
+forecast_start_times <- expand.grid(dates, cycles, sites) |> 
   stats::setNames(c("date", "cycle", "site_id")) |> 
-  mutate(start_times = paste(date, cycle),
-         cycle = stringr::str_pad(lubridate::hour(lubridate::as_datetime(start_times)),2, "left", 0),
+  mutate(start_times = paste0(date, " ", cycle, ":00:00"),
          dir = file.path(base_dir, model_name, site_id, date, cycle)) |> 
   select(site_id, start_times, dir)
-
 
 files_present <- purrr::map_int(1:nrow(forecast_start_times), function(i, forecast_start_times){
   if(fs::dir_exists(forecast_start_times$dir[i])){
@@ -55,26 +48,26 @@ forecast_start_times <- bind_cols(forecast_start_times, files_present) |>
   filter(is.na(files_present) | files_present < 31 | reprocess_all)
 
 
-future::plan("future::multisession", workers = 4)
+#future::plan("future::multisession", workers = 4)
 
 purrr::walk(1:nrow(forecast_start_times),
-                   function(i, forecast_start_times, df, model_name, base_dir){
-                     df |> 
-                       dplyr::filter(start_time == lubridate::as_datetime(forecast_start_times$start_times[i]),
-                                     variable %in% c("PRES","TMP","RH","UGRD","VGRD","APCP","DSWRF","DLWRF"),
-                                     site_id == forecast_start_times$site_id[i]) |> 
-                       collect() |> 
-                       disaggregate_fluxes() |> 
-                       add_horizon0_time() |> 
-                       convert_precip2rate() |> 
-                       disaggregate2hourly() |> 
-                       write_noaa_gefs_netcdf(dir = base_dir, model_name = model_name, add_directory = TRUE)
-                   },
-                   df = df,
-                   forecast_start_times= forecast_start_times,
-                   model_name = model_name,
-                   base_dir = base_dir
-                   
+            function(i, forecast_start_times, df, model_name, base_dir){
+              df |> 
+                dplyr::filter(start_time == lubridate::as_datetime(forecast_start_times$start_times[i]),
+                              variable %in% c("PRES","TMP","RH","UGRD","VGRD","APCP","DSWRF","DLWRF"),
+                              site_id == forecast_start_times$site_id[i]) |> 
+                collect() |> 
+                disaggregate_fluxes() |> 
+                add_horizon0_time() |> 
+                convert_precip2rate() |> 
+                disaggregate2hourly() |> 
+                write_noaa_gefs_netcdf(dir = base_dir, model_name = model_name, add_directory = TRUE)
+            },
+            df = df,
+            forecast_start_times= forecast_start_times,
+            model_name = model_name,
+            base_dir = base_dir
+            
 )
 
 forecast |> 
