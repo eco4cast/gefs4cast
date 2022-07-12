397c29073386c0f90048cc665dd43ff594853e66
rqthomas <rqthomas@vt.edu>
2022-07-12 15:58:54 -0400

    creating site list in stack function

--- stack4casts.R
+++ stack4casts.R
@@ -11,13 +11,28 @@ s3 <- arrow::s3_bucket("drivers/noaa/neon/gefs",
 
 df <- arrow::open_dataset(s3)
 
-df |> dplyr::filter(variable %in% c("PRES","TMP","RH","UGRD","VGRD","APCP","DSWRF","DLWRF"),
-                    site_id == "BART") |> 
+sites <- df |> 
+  dplyr::filter(start_time == lubridate::as_datetime(dates[1]),
+                variable %in% c("PRES","TMP","RH","UGRD","VGRD","APCP","DSWRF","DLWRF")) |> 
+  distinct(site_id) |> 
+  collect() |> 
+  pull(site_id)
+
+d <- df |> dplyr::filter(variable == "PRES",
+                    site_id == "BART",
+                    horizon %in% c(0,3,6)) |> 
   distinct(start_time) |> collect()
 
+sites <- df |> 
+  dplyr::filter(start_time == lubridate::as_datetime(d[1]),
+                variable == "PRES") |> 
+  distinct(site_id) |> 
+  collect() |> 
+  pull(site_id)
+
 forecast <- df |> 
-  filter(start_time >= lubridate::as_datetime("2022-04-17 00:00:00"),
-         start_time <= lubridate::as_datetime("2022-04-19 00:00:00"),
+  filter(start_time >= lubridate::as_datetime("2020-09-25 00:00:00"),
+         start_time <= max(d$start_time),
          variable %in% c("PRES","TMP","RH","UGRD","VGRD","APCP","DSWRF","DLWRF"),
          site_id == "BART",
          horizon %in% c(0,3,6)) |> 
