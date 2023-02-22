19545b8c43d9308566f6b0902be3fe1b3780078c
Carl <cboettig@gmail.com>
2023-02-22 16:25:13 +0000

    :construction:

--- inst/cog-examples/efi-snapshots.R
+++ inst/cog-examples/efi-snapshots.R
@@ -15,26 +15,14 @@ gdalcubes_options(parallel=TRUE)
 
 devtools::load_all()
 library(arrow)
-endpoint <- "https://sdsc.osn.xsede.org"
-bucket <- "bio230014-bucket01/neon4cast-drivers/noaa/gefs-v12/stage1-stats/"
-s3 <- arrow::S3FileSystem$create(endpoint_override = endpoint,
-                                 access_key = Sys.getenv("OSN_KEY"),
-                                 secret_key = Sys.getenv("OSN_SECRET"))
-s3_dir <- arrow::SubTreeFileSystem$create(bucket, s3)
 
-sf_sites <- neon_sites()
-#ensemble <-  paste0("gep", stringr::str_pad(1:30, 2, pad="0"))
-ensemble <- c(mu = "geavg", sigma = "gespr") # mean and spread
 
-cores <- length(ensemble)
-dates <- seq(as.Date("2022-01-01"), Sys.Date()-1, by=1)
-bench::bench_time({
-  for(date in dates) {
-    dfs <- lapply(ensemble, grib_extract, date = date, sites = sf_sites)
-    dfs |> efi_format_cubeextract(date = date, sf_sites = sf_sites) |>
-    dplyr::mutate(family = "normal") |>
-    arrow::write_dataset(s3_dir,
-                       partitioning = c("reference_datetime", "site_id"))
-  }
-})
+# can start back to 2017-01-01
+gefs_to_parquet(dates = seq(as.Date("2022-01-01"), Sys.Date()-1, by=1),
+                ensemble = c(mu = "geavg", sigma = "gespr"),
+                gefs_s3_dir("stage1-stats"))
+
+gefs_to_parquet(dates = seq(as.Date("2022-01-01"), Sys.Date()-1, by=1),
+                ensemble = c("gec00", paste0("gep", stringr::str_pad(1:30, 2, pad="0"))),
+                gefs_s3_dir("stage1"))
 
