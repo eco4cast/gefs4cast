d3be369db7d0dd7fea262a4fe9bbc96226fe9886
Carl <cboettig@gmail.com>
2023-02-25 04:10:39 +0000

    update docs

--- R/gdalcubes-to-parquet.R
+++ R/gdalcubes-to-parquet.R
@@ -9,9 +9,12 @@ gefs_s3_dir <- function(path) {
   s3_dir <- arrow::SubTreeFileSystem$create(bucket, s3)
 }
 
+
 #' gefs_to_parquet
 #'
 #' @param dates a vector of reference_datetimes
+#' @param ensemble vector of ensemble values (e.g. 'gep01', 'gep02', ...)
+#' @param s3_dir path to S3 directory
 #' @inheritParams grib_extract
 gefs_to_parquet <- function(dates,
                             ensemble,
@@ -46,21 +49,25 @@ gefs_to_parquet <- function(dates,
 #' @param dfs list of tables from grib_extract
 #' @param date forecast reference_datetime
 #' @param sites sites (as sf object)
+#' @param bands named vector of band numbers, see [gefs_bands()]
 #' @export
 efi_format_cubeextract <- function(dfs,
                                    date,
-                                   sf_sites = neon_sites(),
+                                   sites = neon_sites(),
                                    bands = gefs_bands()) {
 
-  sites <- sf_sites |>
+  sites_df <- sites |>
     tibble::as_tibble() |>
     dplyr::select(FID, site_id)
   vars <- names(bands)
 
+
+  FID <- datetime <- time <- NULL # globalVariables has side effect?
+
   df <-
     purrr::list_rbind(dfs, names_to = "parameter") |>
     tibble::as_tibble() |>
-    dplyr::inner_join(sites) |>
+    dplyr::inner_join(sites_df) |>
     dplyr::rename({bands}) |>
     dplyr::mutate(datetime = lubridate::as_datetime(time)) |>
     dplyr::select(-FID) |>
