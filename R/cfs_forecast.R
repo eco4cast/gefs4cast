1bd31ee6887903c57935ceddeac35c935c3731a5
Carl <cboettig@gmail.com>
2023-03-29 05:09:23 +0000

    assert gdal 3.6.0

--- R/cfs_forecast.R
+++ R/cfs_forecast.R
@@ -11,6 +11,18 @@
 #band 40: pressure (surface)
 # soilW
 
+cfs_s3_dir <- function(product,
+                        path = "neon4cast-drivers/noaa/cfs/",
+                        endpoint = "https://sdsc.osn.xsede.org",
+                        bucket = paste0("bio230014-bucket01/", path, product))
+{
+
+  s3 <- arrow::S3FileSystem$create(endpoint_override = endpoint,
+                                   access_key = Sys.getenv("OSN_KEY"),
+                                   secret_key = Sys.getenv("OSN_SECRET"))
+  s3_dir <- arrow::SubTreeFileSystem$create(bucket, s3)
+  s3_dir
+}
 
 
 
@@ -38,7 +50,7 @@ cfs_url <- function(datetime,
 
 
 cfs_horizon <- function(reference_datetime = Sys.Date()-2,
-                        horizon=lubridate::days(273)) {
+                        horizon=lubridate::days(200)) {
 
   start <- lubridate::as_datetime(reference_datetime)
   n = 1 + ( horizon / lubridate::hours(6) )
@@ -47,14 +59,23 @@ cfs_horizon <- function(reference_datetime = Sys.Date()-2,
   out[-1] # first horizon has different bands
 }
 
+assert_gdal_version <- function(version = "3.6.0") {
+  gdal_version <- sf:::CPL_gdal_version()
+  if(!compareVersion(gdal_version, version)>=0) {
+    stop(paste("gdal v", version,
+               "is required, but detected gdal v", gdal_version), call. = FALSE)
+  }
+}
+
 
 cfs_stars_extract <- function(ens,
                               reference_datetime = Sys.Date()-1,
-                              horizon = lubridate::days(273),
+                              horizon = lubridate::days(200),
                               cycle = "00",
                               interval="6hrly",
                               sites = neon_sites(),
                               ...) {
+  assert_gdal_version()
   reference_datetime <- lubridate::as_date(reference_datetime)
 
   date_times <- cfs_horizon(reference_datetime, horizon)
@@ -122,7 +143,7 @@ extract_sites_ <- function(r, sites) {
 
 cfs_urls <- function(ens = 1,
                      reference_datetime=Sys.Date()-2,
-                     horizon = lubridate::days(273),
+                     horizon = lubridate::days(200),
                      cycle = "00",
                      interval = "6hrly") {
   cfs_horizon(reference_datetime,horizon) |>
@@ -132,7 +153,7 @@ cfs_urls <- function(ens = 1,
 
 cfs_grib_collection <- function(ens,
                                 date = Sys.Date()-1,
-                                horizon = lubridate::days(273),
+                                horizon = lubridate::days(200),
                                 cycle = "00",
                                 interval="6hrly",
                                 ...) {
