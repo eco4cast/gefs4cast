c18402a1a8845de7fcf2856fa38a9b0eacc61980
Carl Boettiger <cboettig@gmail.com>
2023-04-01 23:40:06 +0000

    starting stars-based comparison

--- R/cfs_forecast.R
+++ R/cfs_forecast.R
@@ -121,21 +121,31 @@ select_bands_ <- function(r, bands){
   r[,,,unname(bands)]
 }
 
-extract_sites_ <- function(r, sites) {
+# a `stars` based extract_sites mimicking gdalcubes:
+# automatically handle
+extract_sites_ <- function(r, sites, variable_dimension = 3) {
 
+  # better to match crs ahead of time, but can do on-the-fly
   if (!identical(sf::st_crs(r), sf::st_crs(sites))) {
     sites <- sf::st_transform(sites, st_crs(r))
   }
 
+  # stars::st_extract
   y <- stars::st_extract(r, sf::st_coordinates(sites))
 
-  variables <- stars::st_get_dimension_values(r,3)
+  # variable name parsing from grib idx, not generic
+  variables <- stars::st_get_dimension_values(r,variable_dimension)
   variables <- gsub("(\\w+:\\w+):.*", "\\1", variables)
   colnames(y) <- variables
 
-  vctrs::vec_cbind(y, sites) |>
-    tibble::as_tibble() |>
-    tidyr::pivot_longer(variables,
+  # format nicely
+  out <- sites |>
+    dplyr::select("site_id", "geometry") |>
+    vctrs::vec_cbind(y) |>
+    tibble::as_tibble() # should we leave it as an sf object?
+
+  # convert to EFI 'long' format while variable names are handy
+  out |> tidyr::pivot_longer(variables,
                         names_to="variable",
                         values_to="prediction")
 }
---
+++ R/gefs_stars_extract.R
@@ -0,0 +1,31 @@
+gefs_stars_extract <- function(ens,
+                              reference_datetime = Sys.Date()-1,
+                              horizon = gefs_horizon(),
+                              bands = gefs_bands(),
+                              cycle = "00",
+                              sites = neon_sites(),
+                              ...) {
+  reference_datetime <- lubridate::as_date(reference_datetime)
+  date_times <- cfs_horizon(reference_datetime, horizon)
+  sites <- sites |>
+    sf::st_transform(crs = sf::st_crs(grib_wkt())) |>
+    dplyr::select("site_id", "geometry")
+
+  cfs_extract <- purrr::possibly(function(datetime, quiet=FALSE) {
+    cfs_url(datetime, ens, reference_datetime, cycle, interval) |>
+      stars::read_stars() |>
+      select_bands_(bands) |>
+      extract_sites_(sites) |>
+      dplyr::mutate(parameter = ens,
+                    datetime = datetime,
+                    reference_datetime = reference_datetime,
+                    family="ensemble")
+  })
+
+  parallel::mclapply(date_times,
+                     cfs_extract,
+                     mc.cores = getOption("mc.cores", 1L)
+  ) |>
+    purrr::list_rbind()
+
+}
