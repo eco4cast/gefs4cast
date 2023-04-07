d246a532929847704ed555ffc3ca6e4de9cde764
Carl Boettiger <cboettig@gmail.com>
2023-04-07 23:24:09 +0000

    metadata

--- inst/cog-examples/cfs_examples.R
+++ inst/cog-examples/cfs_examples.R
@@ -1,41 +1,25 @@
 
-
+library(gdalcubes)
+gdalcubes_options(parallel=TRUE)
 devtools::load_all()
+#vis4cast::ignore_sigpipe()
+#options("mc.cores"=parallel::detectCores())
+
+# c6in.4xlarge:
+# cirrus: 5 min all 128 cores 2x overload or 2
+dates <- Sys.Date()-3 # seq(as.Date("2023-04-04"), as.Date("2023-04-06"), by=1)
+gdalcubes_options(parallel=2*parallel::detectCores())
+
+s3 <- "cfs_parquet" # cfs_s3_dir("6hrly/00")
+bench::bench_time({
+  cfs_to_parquet(dates,  horizon = cfs_horizon, path = s3)
+})
 
 options("mc.cores"=parallel::detectCores())
-gdalcubes_cloud_config()
-
 bench::bench_time({
   cfs_stars(Sys.Date()-1,  horizon = cfs_horizon)
 })
 
 
-ens <- 1
-reference_datetime <- as.Date("2023-03-02")
-#sites <- neon_sites()
-sites <- neon_sites() |> sf::st_shift_longitude()
-horizon = cfs_horizon(ens, reference_datetime)
-h <- horizon[[2]]
 
-h <- gefs_horizon()[[2]]
-gefs_urls("geavg", reference_datetime, horizon=h)
-cfs_url(ens = ens,
-        reference_datetime = reference_datetime,
-        horizon = h)
 
-options("mc.cores"=parallel::detectCores())
-bench::bench_time({ # about 7min on all cores, needs ~ 50 GB, or 4 min per ens in 10 GB
-
-  df <- stars_extract(ens = 1,
-                      reference_datetime = reference_datetime,
-                      sites=sites,
-                      horizon = cfs_horizon,
-                      bands = cfs_band_numbers(),
-                      url_builder = cfs_urls,
-                      cycle = "00")
-
-})
-
-lobstr::obj_size(df)
-
-grib <- cfs_grib_collection(date = reference_datetime, ens=1)
