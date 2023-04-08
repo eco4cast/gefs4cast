ec130e80a1558d28820f3a77b3fa8561176ba546
Carl Boettiger <cboettig@gmail.com>
2023-04-08 07:30:24 +0000

    docs :pen:

--- R/noaa_gefs.R
+++ R/noaa_gefs.R
@@ -5,9 +5,11 @@
 #' @param date reference_datetime
 #' @param cycle start hour of reference datetime
 #' @param s3 s3 bucket or local path
+#' @param ... additional arguments, no longer used but allows compatibility
 #' @export
 noaa_gefs <- function (date = Sys.Date(),
-                       cycle = "00", ...,
+                       cycle = "00",
+                       ...,
                        s3 = arrow::s3_bucket("drivers",
                                              endpoint_override = "data.ecoforecast.org")
 ) {
