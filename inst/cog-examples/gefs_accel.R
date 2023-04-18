ad3a02a89e056e2507c9135a387364b89e63854d
Carl Boettiger <cboettig@gmail.com>
2023-04-18 06:16:04 +0000

    overload time dimension with ensemble, way faster

--- inst/cog-examples/gefs_accel.R
+++ inst/cog-examples/gefs_accel.R
@@ -1,95 +1,89 @@
 reference_datetime <- as.Date("2020-11-22")
 
-library(minio)
-install_mc()
-mc_alias_set("s3",access_key = "", secret_key = "", endpoint = "s3.amazonaws.com")
+devtools::load_all()
+
+gdalcubes::gdalcubes_options(parallel=2*parallel::detectCores())
 
 # No need for /dev/shm on cirrus since network speed < NVME speed
 # AWS machines should use /dev/shm for performance
 shm <- "/dev/shm/noaa"
-shm <- fs::path_expand("~/noaa_shm")
+shm <- "noaa-staging/" # about 16 min on cirrus
 
-# storage
-# shm <- glue::glue("/nvme/noaa-staging/gefs.{date}/{cycle}/atmos/pgrb2ap5/", date = format(reference_datetime, "%Y%m%d"), cycle="00")
-
-bench::bench_time({
-  cmd <- glue::glue("mirror s3/noaa-gefs-pds/gefs.{date}/{cycle}/atmos/pgrb2ap5/ {shm}",
-                    date = format(reference_datetime, "%Y%m%d"), cycle="00")
-  minio::mc(cmd)
-})
-
-devtools::load_all()
-
-
-gdalcubes::gdalcubes_options(parallel=4*parallel::detectCores())
-reference_datetime <- as.Date("2020-11-22")
+stash_local <- function(reference_datetime, shm=tempfile("noaa-staging"), cycle = "00") {
+  minio::install_mc()
+  minio::mc_alias_set("s3",access_key = "", secret_key = "", endpoint = "s3.amazonaws.com")
+  bench::bench_time({
+    cmd <- glue::glue("mirror s3/noaa-gefs-pds/gefs.{date}/{cycle}/atmos/pgrb2ap5/ {shm}",
+                      date = format(reference_datetime, "%Y%m%d"), cycle="00")
+    minio::mc(cmd)
+  })
+}
 
 ensemble <- gefs_ensemble()
 horizon <-  gefs_horizon()
 sites <- neon_sites()
+shm_local <- TRUE
 
 bench::bench_time({
-  nonzero_horiz <- lapply(ensemble, function(ens) {
-    date_time <- reference_datetime + lubridate::hours(horizon)
-    urls <- gefs_urls(ens, reference_datetime, horizon)
-  #  urls <- paste0(shm, basename(urls))
 
-    cube <-
+    # overload time dimension as time + ensemble
+    date_time <- reference_datetime + lubridate::hours(horizon)
+    urls <- purrr::map(ensemble, gefs_urls, reference_datetime, horizon)|> purrr::list_c()
+    url_ensemble <- purrr::map(ensemble, \(i) rep(i, length(horizon))) |> purrr::list_c()
+    fake <- as.Date(Sys.Date()+seq_along(urls))
+    unfake <- tibble::tibble(ensemble = url_ensemble, time=as.character(fake))
+
+    if(shm_local) urls <- paste0(shm, basename(urls))
+    #urls <- gsub(glue::glue("noaa-gefs-pds.s3.amazonaws.com/gefs.{date}/{cycle}/atmos/pgrb2ap5/", date = format(reference_datetime, "%Y%m%d"), cycle="00"), "minio.carlboettiger.info/noaa-staging/", urls)
+    df1 <-
       gdalcubes::stack_cube(urls,
-                            datetime_values = date_time,
-                            band_names = gefs_all_bands())  |> # 5sec
+                            datetime_values = fake,
+                            band_names = gefs_all_bands()) |>
       gdalcubes::select_bands(gefs_bands()) |>
-      gdalcubes::extract_geom(sites)
-  }) #one ensemble member, full horizon: 2.5 min on cirrus, 20sec on c6in.32xlarge
+      gdalcubes::extract_geom(sites) |>
+      tibble::as_tibble() |>
+      dplyr::left_join(unfake) |>
+      dplyr::mutate(time = reference_datetime) |>
+      list() |>
+      efi_format_cubeextract(reference_datetime = reference_datetime,
+                             sites = sites,
+                             bands = gefs_bands())
+
 })
+
+
 # with local sync -- 1min to sync to /dev/shm, 2 min to run on c6in.32xlarge
 
-
 bench::bench_time({
-zero_horiz <- lapply(ensemble, function(ens) {
-  urls <- gefs_urls(ens, reference_datetime, "000")
-  urls <- paste0("/dev/shm/noaa/", basename(urls))
+  urls <- purrr::map_chr(ensemble, function(ens) gefs_urls(ens, reference_datetime, "000"))
 
-  cube0 <-
-    gdalcubes::stack_cube(urls,
-                          datetime_values = reference_datetime,
-                          band_names = gefs_all_bands(zero_horizon = TRUE))|> # 2.5sec
+  if(shm_local) urls <- paste0(shm, basename(urls))
+
+  fake <- as.Date(Sys.Date()+seq_along(ensemble))
+  unfake <- tibble::tibble(ensemble = ensemble, time=as.character(fake))
+  df0 <-
+    gdalcubes::stack_cube(urls, datetime_values = fake,
+                          band_names = gefs_all_bands(zero_horizon = TRUE)) |>
     gdalcubes::select_bands(gefs_bands(TRUE)) |>
-    gdalcubes::extract_geom(sites)
+    gdalcubes::extract_geom(sites) |>
+    tibble::as_tibble() |>
+    dplyr::left_join(unfake) |>
+    dplyr::mutate(time = reference_datetime) |>
+    list() |>
+    efi_format_cubeextract(reference_datetime = reference_datetime,
+                           sites = sites,
+                           bands = gefs_bands(TRUE))
 })
 
-})
+
+family = "ensemble"
+path = "noaa_gefs"
 
 
 
-ens <- gefs_ensemble()
-horizon <-  gefs_horizon()
-date_time <- reference_datetime + lubridate::hours(horizon)
-urls <- gefs_urls(ens, reference_datetime, horizon)
-sites <- neon_sites()
-paths <- gsub("/vsicurl/https://noaa-gefs-pds.s3.amazonaws.com",
-              "s3/noaa-gefs-pds", urls)
+gefs_ens <- dplyr::bind_rows(df1, df0) |> dplyr::mutate(family = family)
+arrow::write_dataset(gefs_ens, path, partitioning=c("reference_datetime", "site_id"))
+
+x <- purrr::list_rbind(list(df1, df0))
 
 
-local_urls <- gsub("noaa-gefs-pds.s3.amazonaws.com/",
-                   "minio.carlboettiger.info/noaa-staging/", urls)
-#x <- stars::read_stars(local_urls)
-
-
-bench::bench_time({ # NVME: 39s thelio, 23 sec cirrus
-  cube <-
-    gdalcubes::stack_cube(local_urls,
-                          datetime_values = date_time,
-                          band_names = gefs_all_bands())  |> # 5sec
-    gdalcubes::select_bands(gefs_bands()) |>
-    gdalcubes::extract_geom(sites)
-})
-
-bench::bench_time({
-  cube <-
-    gdalcubes::stack_cube(urls,
-                          datetime_values = date_time,
-                          band_names = gefs_all_bands())  |> # 5sec
-    gdalcubes::select_bands(gefs_bands()) |>
-    gdalcubes::extract_geom(sites)
-})
