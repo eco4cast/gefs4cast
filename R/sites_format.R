5f968e044f465bee16ffc1b08b1736e797662507
Carl <cboettig@gmail.com>
2023-05-18 16:52:47 +0000

    ensure polygons have site_id

--- R/sites_format.R
+++ R/sites_format.R
@@ -5,6 +5,11 @@ sites_format <- function(sites) {
   if (!("FID" %in% vars)) {
     sites <- tibble::rowid_to_column(sites, "FID")
   }
+
+  if (!("site_id" %in% vars)) {
+    sites <- tibble::rowname_to_column(sites, "site_id")
+  }
+
   if (!inherits(sites, "sf")) {
     longitude <- grepl("^[Ll]ong", vars)
     latitude <- grepl("^[Ll]at", vars)
