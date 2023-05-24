d8272b7edcb0ef3a7896061c798773d29bee0758
Carl <cboettig@gmail.com>
2023-05-24 17:05:57 +0000

    start grabbing TERN sites too

--- inst/cog-examples/gefs_cron.R
+++ inst/cog-examples/gefs_cron.R
@@ -1,8 +1,21 @@
 ## setup
 library(gdalcubes)
 library(gefs4cast)
-#gdalcubes::gdalcubes_options(parallel=2*parallel::detectCores())
-gdalcubes::gdalcubes_options(parallel=TRUE)
+
+gdalcubes::gdalcubes_options(parallel=2*parallel::detectCores())
+#gdalcubes::gdalcubes_options(parallel=TRUE)
+
+
+sites <-
+  dplyr::bind_rows(
+    readr::read_csv(paste0("https://github.com/eco4cast/",
+                           "neon4cast-noaa-download/",
+                           "raw/master/noaa_download_site_list.csv"),
+                    col_select = c("site_id", "latitude", "longitude")),
+    readr::read_csv(paste0("https://github.com/eco4cast/neon4cast-targets/",
+                           "raw/main/tern_field_site_metadata.csv"),
+                    col_select = c("site_id", "latitude", "longitude"))
+  )
 
 
 
@@ -16,7 +29,8 @@ bench::bench_time({ # thelio
   missing_dates <- dates[!(as.character(dates) %in% have_dates)]
   gefs_to_parquet(missing_dates,
                   ensemble=c("geavg", "gespr"),
-                  path = s3)
+                  path = s3,
+                  sites = sites)
 })
 
 message("GEFS v12 pseudo")
@@ -24,7 +38,7 @@ bench::bench_time({ #32xlarge
   s3 <- gefs_s3_dir("pseudo")
   have_dates <- gsub("reference_datetime=", "", s3$ls())
   missing_dates <- dates[!(as.character(dates) %in% have_dates)]
-  gefs4cast:::gefs_pseudo_measures(missing_dates,  s3)
+  gefs4cast:::gefs_pseudo_measures(missing_dates,  path = s3, sites = sites)
 })
 
 message("GEFS v12 stage1")
@@ -32,7 +46,7 @@ bench::bench_time({ # cirrus ~ 6days for full set
   s3 <- gefs_s3_dir("stage1")
   have_dates <- gsub("reference_datetime=", "", s3$ls())
   missing_dates <- dates[!(as.character(dates) %in% have_dates)]
-  gefs_to_parquet(missing_dates, path = s3)
+  gefs_to_parquet(missing_dates, path = s3, sites = sites)
 })
 
 
