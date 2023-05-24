8c309b2b81a393365a522410e00921252e4e082d
Carl <cboettig@gmail.com>
2023-05-24 16:05:34 +0000

    grep

--- R/sites_format.R
+++ R/sites_format.R
@@ -11,8 +11,8 @@ sites_format <- function(sites) {
   }
 
   if (!inherits(sites, "sf")) {
-    longitude <- grepl("^[Ll]ong", vars)
-    latitude <- grepl("^[Ll]at", vars)
+    longitude <- grep("^[Ll]ong", vars)
+    latitude <- grep("^[Ll]at", vars)
     sites <- sf::st_as_sf(sites, coords=c(longitude,latitude), crs=4326)
   }
   sites
